fe8544aa680c81fcf90eaf1f729c5b049f9b7bd3
yuuki1293 <yuuki1293@outlook.jp>
2024-03-18 13:02:14 +0900

    ドキュメントを作成

--- MultipassCell.Core/Mirror.fs
+++ MultipassCell.Core/Mirror.fs
@@ -3,5 +3,15 @@
 open FSharp.Stats
 
 type Mirror<'T> =
-    abstract reflect: Vector<'T> -> Vector<'T> -> Vector<'T>
-    abstract normal: Vector<'T> -> Vector<'T>
+    /// <summary>
+    /// Calculate the vector of the reflected laser.
+    /// </summary>
+    /// <param name="p0">starting point</param>
+    /// <param name="r0">incidence vector</param>
+    abstract reflect: p0:Vector<'T> -> r0:Vector<'T> -> Vector<'T>
+    
+    /// <summary>
+    /// Normal vector at a point.
+    /// </summary>
+    /// <param name="p">point</param>
+    abstract normal: p:Vector<'T> -> Vector<'T>
