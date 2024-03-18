be483395de6f184b55140bd68cc7aef7d565d3ca
yuuki1293 <yuuki1293@outlook.jp>
2024-03-18 12:01:25 +0900

    計算できた

--- MultipassCell.Core/Common/Sphere.fs
+++ MultipassCell.Core/Common/Sphere.fs
@@ -6,31 +6,33 @@ open FSharp.Stats.Vector.Generic
 
 type Sphere(xr: float, yr: float, zr: float, r: float) =
     interface Mirror<float> with
-        member _.reflect o r0 =
-            let x0 = o.Values[0]
-            let y0 = o.Values[1]
-            let z0 = o.Values[2]
-            let dx0 = r0.Values[0]
-            let dy0 = r0.Values[1]
-            let dz0 = r0.Values[2]
-            let a = (pown dx0 2) + (pown dy0 2) + (pown dz0 2)
+        member _.reflect p0 r0 =
+            let pow2 x = pown x 2
+            let size v = map (fun x -> x * x) v |> sum |> sqrt
+            let unitV v = let s = size v in map (fun x -> x / s) v
+            
+            let x0 = p0[0]
+            let y0 = p0[1]
+            let z0 = p0[2]
+            let dx0 = r0[0]
+            let dy0 = r0[1]
+            let dz0 = r0[2]
+            let a = pow2 dx0 + pow2 dy0 + pow2 dz0
             let b = -2. * (
-                + dx0 * x0 - dx0 * xr
-                + dy0 * y0 - dy0 * yr
-                + dz0 * z0 - dz0 * zr)
+                dx0 * (xr - x0)
+                + dy0 * (yr - y0)
+                + dz0 * (zr - z0))
             let c =
-                (pown x0 2) + (pown y0 2) + (pown z0 2)
-                - 2. * (x0 * xr + y0 * yr + z0 * zr) 
-                + (pown xr 2) + (pown yr 2) + (pown zr 2)
-                - pown r 2
-            let t = (- b + sqrt ((pown b 2) - 4. * a * c)) / (2. * a)
-            let t_ = (- b - sqrt ((pown b 2) - 4. * a * c)) / (2. * a)
-            let x1 = x0 - dx0 * t
-            let y1 = y0 - dy0 * t
-            let z1 = z0 - dz0 * t
-            let xv = x1 - xr
-            let yv = y1 - yr
-            let zv = z1 - zr
-            let n = vector [xv; yv; zv]
+                pow2 (xr - x0) + pow2 (yr - y0) + pow2 (zr - z0)
+                - pow2 r
+            let t = (- b + sqrt ((pow2 b) - 4. * a * c)) / (2. * a)
+            let t_ = (- b - sqrt ((pow2 b) - 4. * a * c)) / (2. * a)
+            let x1 = x0 + dx0 * t
+            let y1 = y0 + dy0 * t
+            let z1 = z0 + dz0 * t
+            let xv = xr - x1
+            let yv = yr - y1
+            let zv = zr - z1
+            let n = unitV (vector [xv; yv; zv])
             let r1 = r0 - 2. * n * (dot n r0)
             r1
--- MultipassCell.Test/Program.fs
+++ MultipassCell.Test/Program.fs
@@ -1,2 +1,10 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open MultipassCell.Core
+open MultipassCell.Core.Common
+open FSharp.Stats
+
+let sphere = Sphere(0, 0, 0, 3)
+
+let p0 = vector [1; 2; 3]
+let r0 = vector [-1; -1; -1]
+(sphere :> Mirror<float>).reflect p0 r0
+|> printfn "%A"
