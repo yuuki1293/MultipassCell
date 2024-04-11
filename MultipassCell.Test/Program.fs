bb3fa1fcd11e797f569b6db2cfe7ffc9f3626e28
yuuki1293 <yuuki1293@outlook.jp>
2024-04-11 15:36:05 +0900

    戻って来るレーザー

--- MultipassCell.Test/Program.fs
+++ MultipassCell.Test/Program.fs
@@ -3,14 +3,14 @@ open MultipassCell.Core.Common
 open FSharp.Stats
 open Plotly.NET
 
-let sphere_r = Sphere(0, 0, -4.79795897113, 10, 1)
-let sphere_l = Sphere(0, 0, 4.79795897113, 10, -1)
-let p0 = vector [2; 0; -4.79795897113]
-let r0 = vector [-2; 2; 9.59591794226]
+let sphere_r = Sphere(0, 0, -4, 9, 1)
+let sphere_l = Sphere(0, 0, 4, 9, -1)
+let p0 = vector [4.1231; 0; -4]
+let r0 = vector [4.1231; 4.1231; -8]
 
 let mutable laser = [(p0, r0)]
 
-for _ in [0..2] do
+for _ in [0..49] do
     laser <- reflect sphere_r laser.Head :: laser
     // printfn $"%A{laser}"
     laser <- reflect sphere_l laser.Head :: laser
