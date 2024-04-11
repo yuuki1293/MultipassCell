c7358a1a954404f8febad441b24e58a0e2603167
yuuki1293 <yuuki1293@outlook.jp>
2024-04-11 15:23:19 +0900

    6点で動かす

--- MultipassCell.Test/Program.fs
+++ MultipassCell.Test/Program.fs
@@ -3,14 +3,14 @@ open MultipassCell.Core.Common
 open FSharp.Stats
 open Plotly.NET
 
-let sphere_r = Sphere(0, 0, -500, 1000, 1)
-let sphere_l = Sphere(0, 0, 500, 1000, -1)
-let p0 = vector [200; 0; -479]
-let r0 = vector [-200; 200; 959.6]
+let sphere_r = Sphere(0, 0, -4.79795897113, 10, 1)
+let sphere_l = Sphere(0, 0, 4.79795897113, 10, -1)
+let p0 = vector [2; 0; -4.79795897113]
+let r0 = vector [-2; 2; 9.59591794226]
 
 let mutable laser = [(p0, r0)]
 
-for _ in [0..100] do
+for _ in [0..2] do
     laser <- reflect sphere_r laser.Head :: laser
     // printfn $"%A{laser}"
     laser <- reflect sphere_l laser.Head :: laser
@@ -20,5 +20,5 @@ Chart.Point3D(
     x = [ for p, _ in laser -> p[0]],
     y = [ for p, _ in laser -> p[1]],
     z = [ for p, _ in laser -> p[2]]
-    )
+)
 |> Chart.show
