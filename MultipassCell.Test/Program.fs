d95572376d5a0d42e435c56d2d13a5ef186eea29
yuuki1293 <yuuki1293@outlook.jp>
2024-04-12 15:28:12 +0900

    グラフサイズの変更

--- MultipassCell.Test/Program.fs
+++ MultipassCell.Test/Program.fs
@@ -2,10 +2,11 @@
 open MultipassCell.Core.Common
 open FSharp.Stats
 open Plotly.NET
+open Plotly.NET.LayoutObjects
 
 let sphere_r = Sphere(0, 0, -4, 9, 1)
 let sphere_l = Sphere(0, 0, 4, 9, -1)
-let p0 = vector [4.1231; 0; -4]
+let p0 = vector [5.1231; 0; -4]
 let r0 = vector [4.1231; 4.1231; -8]
 
 let mutable laser = [(p0, r0)]
@@ -16,9 +17,10 @@ for _ in [0..49] do
     laser <- reflect sphere_l laser.Head :: laser
     // printfn $"%A{laser}"
     
-Chart.Point3D(
+Chart.Line3D(
     x = [ for p, _ in laser -> p[0]],
     y = [ for p, _ in laser -> p[1]],
     z = [ for p, _ in laser -> p[2]]
 )
+|> Chart.withSize(1000, 1000)
 |> Chart.show
