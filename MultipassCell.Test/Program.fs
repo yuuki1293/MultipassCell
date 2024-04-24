1dfd57da82f8b98de2fdcd31c52f68af4f0ed340
yuuki1293 <yuuki1293@outlook.jp>
2024-04-24 13:05:49 +0900

    5点反射させて戻って来る

--- MultipassCell.Test/Program.fs
+++ MultipassCell.Test/Program.fs
@@ -5,7 +5,7 @@ open Plotly.NET
 open Plotly.NET.LayoutObjects
 open Plotly.NET.StyleParam
 
-let refCount = 50
+let refCount = 5
 let sphere_r = Sphere(0.1, 0, -4, 9, 1)
 let sphere_l = Sphere(0, 0, 4, 9, -1)
 let p0 = vector [0.1493476; 0; -4.998761]
@@ -23,7 +23,7 @@ proceed(refCount, true)
 let cameraEye = CameraEye.init(1.25, 1.25, -1.25)
 let cameraUp = CameraUp.init(5, 0, 0)
 let camera = Camera.init(Eye = cameraEye, Up = cameraUp)
-Chart.Point3D(
+Chart.Line3D(
     x = [ for p, _ in laser -> p[0]]
     ,y = [ for p, _ in laser -> p[1]]
     ,z = [ for p, _ in laser -> p[2]]
