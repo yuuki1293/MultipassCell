95924d38489e0fd79505443424229e9527bd078f
yuuki1293 <yuuki1293@outlook.jp>
2024-04-24 12:48:42 +0900

    反射5回のパラメータ

--- MultipassCell.Test/Program.fs
+++ MultipassCell.Test/Program.fs
@@ -3,29 +3,34 @@ open MultipassCell.Core.Common
 open FSharp.Stats
 open Plotly.NET
 open Plotly.NET.LayoutObjects
+open Plotly.NET.StyleParam
 
-let sphere_r = Sphere(0, 0, -4, 9, 1)
+let refCount = 50
+let sphere_r = Sphere(0.1, 0, -4, 9, 1)
 let sphere_l = Sphere(0, 0, 4, 9, -1)
-let p0 = vector [5.1231; 0; -4]
-let r0 = vector [4.1231; 4.1231; -8]
+let p0 = vector [0.1493476; 0; -4.998761]
+let r0 = vector [0.1493476+0.1221910; 0; -4.998761-4.997257]
 
 let mutable laser = [(p0, r0)]
 
-for _ in [0..49] do
-    laser <- reflect sphere_r laser.Head :: laser
-    // printfn $"%A{laser}"
-    laser <- reflect sphere_l laser.Head :: laser
-    // printfn $"%A{laser}"
+let rec proceed = function
+    | 0, _ -> ()
+    | count, true -> laser <- reflect sphere_r laser.Head :: laser; proceed (count-1, false)
+    | count, false -> laser <- reflect sphere_l laser.Head :: laser; proceed (count-1, true)
+
+proceed(refCount, true)
 
 let cameraEye = CameraEye.init(1.25, 1.25, -1.25)
 let cameraUp = CameraUp.init(5, 0, 0)
 let camera = Camera.init(Eye = cameraEye, Up = cameraUp)
-let scene = Scene.init(Camera = camera)
-Chart.Line3D(
-    x = [ for p, _ in laser -> p[0]],
-    y = [ for p, _ in laser -> p[1]],
-    z = [ for p, _ in laser -> p[2]]
+Chart.Point3D(
+    x = [ for p, _ in laser -> p[0]]
+    ,y = [ for p, _ in laser -> p[1]]
+    ,z = [ for p, _ in laser -> p[2]]
+    ,Camera = camera
 )
+// |> Chart.withSceneStyle(AspectMode = AspectMode.Data)
 |> Chart.withSize(1000, 1000)
-|> Chart.withScene(scene)
 |> Chart.show
+
+System.Console.ReadLine()
