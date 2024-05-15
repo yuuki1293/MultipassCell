9d46ead0022c295842eae82a7148f0ad077375a0
yuuki1293 <yuuki1293@outlook.jp>
2024-05-15 13:35:57 +0900

    reflectを改善

--- MultipassCell.Core/Common/Sphere.fs
+++ MultipassCell.Core/Common/Sphere.fs
@@ -23,12 +23,8 @@ type Sphere(xr: float, yr: float, zr: float, r: float, sign_: int) =
     
     interface Mirror<float> with
         member this.reflect p0 r0 =
-            let x0 = p0[0]
-            let y0 = p0[1]
-            let z0 = p0[2]
-            let dx0 = r0[0]
-            let dy0 = r0[1]
-            let dz0 = r0[2]
+            let [|x0; y0; z0|] = p0.ToArray()
+            let [|dx0; dy0; dz0|] = r0.ToArray()
             let a = pow2 dx0 + pow2 dy0 + pow2 dz0
             let b = -2. * (
                 dx0 * (xr - x0)
@@ -40,14 +36,9 @@ type Sphere(xr: float, yr: float, zr: float, r: float, sign_: int) =
             let t = (- b + sqrt ((pow2 b) - 4. * a * c)) / (2. * a)
             let t_ = (- b - sqrt ((pow2 b) - 4. * a * c)) / (2. * a)
             let t = choice z0 dz0 t t_
-            let x1 = x0 + dx0 * t
-            let y1 = y0 + dy0 * t
-            let z1 = z0 + dz0 * t
-            let p1 = vector [x1; y1; z1]
-            let xv = xr - x1
-            let yv = yr - y1
-            let zv = zr - z1
-            let n = (this :> Mirror<float>).normal (vector [xv; yv; zv])
+            let p1 = p0 + r0 * t
+            let pv = vector [xr; yr; zr] - p1
+            let n = (this :> Mirror<float>).normal pv
             let r1 = r0 - 2. * n * (dot n r0)
             (p1, r1)
 
