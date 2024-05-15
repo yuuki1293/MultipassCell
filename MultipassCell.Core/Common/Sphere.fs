82c58c6f074cd4b7811e948a76dafdfa310c5743
yuuki1293 <yuuki1293@outlook.jp>
2024-05-15 14:31:25 +0900

    ベクトルを分解しない

--- MultipassCell.Core/Common/Sphere.fs
+++ MultipassCell.Core/Common/Sphere.fs
@@ -6,7 +6,7 @@ open MultipassCell.Core
 open FSharp.Stats
 open FSharp.Stats.Vector.Generic
 
-type Sphere(xr: float, yr: float, zr: float, r: float, sign_: int) =
+type Sphere(pr: Vector<float>, r: float, sign_: int) =
     let pow2 x = pown x 2
     let size v = map (fun x -> x * x) v |> sum |> sqrt
     let unitV v = let s = size v in map (fun x -> x / s) v
@@ -15,29 +15,23 @@ type Sphere(xr: float, yr: float, zr: float, r: float, sign_: int) =
         else
             let z = z0 + dz0 * t
             if sign z = sign_ then t else t_
+    let selfDot vec = dot vec vec
             
     new (args: float list) =
         match args with
-        | xr::yr::zr::r::sign_::_ -> Sphere(float xr, float yr, float zr, float r, int sign_)
-        | _ -> failwith "too few arguments"; Sphere(0., 0., 0., 0., 0) 
+        | xr::yr::zr::r::sign_::_ -> Sphere(vector [float xr; float yr; float zr], float r, int sign_)
+        | _ -> failwith "too few arguments"; Sphere(Vector.zeroCreate 3, 0., 0)
     
     interface Mirror with
         member this.reflect p0 r0 =
-            let [|x0; y0; z0|] = p0.ToArray()
-            let [|dx0; dy0; dz0|] = r0.ToArray()
-            let a = pow2 dx0 + pow2 dy0 + pow2 dz0
-            let b = -2. * (
-                dx0 * (xr - x0)
-                + dy0 * (yr - y0)
-                + dz0 * (zr - z0))
-            let c =
-                pow2 (xr - x0) + pow2 (yr - y0) + pow2 (zr - z0)
-                - pow2 r
+            let a = selfDot r0
+            let b = -2. * (dot r0 (pr - p0))
+            let c = selfDot (pr - p0) - pow2 r
             let t = (- b + sqrt ((pow2 b) - 4. * a * c)) / (2. * a)
             let t_ = (- b - sqrt ((pow2 b) - 4. * a * c)) / (2. * a)
-            let t = choice z0 dz0 t t_
+            let t = choice p0[2] r0[2] t t_
             let p1 = p0 + r0 * t
-            let pv = vector [xr; yr; zr] - p1
+            let pv = pr - p1
             let n = (this :> Mirror).normal pv
             let r1 = r0 - 2. * n * (dot n r0)
             (p1, r1)
@@ -46,8 +40,8 @@ type Sphere(xr: float, yr: float, zr: float, r: float, sign_: int) =
         
         member _.solve v =
             let x, y = v[0], v[1]
-            let z = float sign_ * sqrt(pow2 r - pow2(x - xr) - pow2(y - yr)) + zr
-            vector [|x; y; z|]
+            let z = float sign_ * sqrt(pow2 r - pow2(x - pr[0]) - pow2(y - pr[1])) + pr[2]
+            vector [x; y; z]
         
     interface Surface with
         member this.surface resolution corner1 corner2 =
@@ -64,7 +58,7 @@ type Sphere(xr: float, yr: float, zr: float, r: float, sign_: int) =
                     for x in xs do
                         yield seq {
                             for y in ys do
-                                yield float sign_ * sqrt(pow2 r - pow2(x - xr) - pow2(y - yr)) + zr
+                                yield float sign_ * sqrt(pow2 r - pow2(x - pr[0]) - pow2(y - pr[1])) + pr[2]
                         }
                 }
             
