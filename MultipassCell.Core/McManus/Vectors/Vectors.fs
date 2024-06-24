1675984e49e4f81cec9b17e3d6c1c4eff0d06226
yuuki1293 <yuuki1293@outlook.jp>
2024-06-24 11:45:51 +0900

    McManusのベクトルと行列を定義

---
+++ MultipassCell.Core/McManus/Vectors/Vectors.fs
@@ -0,0 +1,27 @@
+namespace MultipassCell.Core.McManus
+
+open FSharp.Stats
+
+type Vectors =
+    static member X x y z dx dy dz = vector [| x; dx / dz; y; dy / dz |]
+
+    static member D d =
+        matrix [| [| 1.; d; 0.; 0. |]
+                  [| 0.; 1.; 0.; 0. |]
+                  [| 0.; 0.; 1.; d |]
+                  [| 0.; 0.; 0.; 1. |] |]
+
+    static member R gx gy =
+        matrix [| [| 1.; 0.; 0.; 0. |]
+                  [| -gx; 1.; 0.; 0. |]
+                  [| 0.; 0.; 1.; 0. |]
+                  [| 0.; 0.; -gy; 1. |] |]
+
+    static member t t =
+        let c = cos t
+        let s = sin t
+
+        matrix [| [| c; 0.; s; 0. |]
+                  [| 0.; c; 0.; s |]
+                  [| -s; 0.; c; 0. |]
+                  [| 0.; -s; 0.; c |] |]
